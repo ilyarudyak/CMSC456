543d8994dab0f8c4a9580ebddbb96dcaa0f4ac71
Ilya Rudyak <ilyarudyak@yahoo.com>
2015-01-28 23:54:28 +0300

    done pa1-4

---
+++ pa4_katz/src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ilyarudyak on 21/01/15.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        String s = "hello my friend";
+        System.out.println(Utility.byteArrayToString(Utility.stringToByteArray(s)));
+
+    }
+}
